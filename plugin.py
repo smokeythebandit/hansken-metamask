4cb89c96472bda0c4bc0a0e509ed378c2b266fcd
Benjamin Martens <benjamin.martens@pm.me>
2022-11-29 13:55:08 +0000

    Now extracting ethereum address from persist-root file

--- plugin.py
+++ plugin.py
@@ -1,7 +1,10 @@
+import logging
+
 from hansken_extraction_plugin.api.extraction_plugin import ExtractionPlugin
 from hansken_extraction_plugin.api.plugin_info import Author, MaturityLevel, PluginId, PluginInfo
 from hansken_extraction_plugin.runtime.extraction_plugin_runner import run_with_hanskenpy
 from logbook import Logger
+import json
 
 log = Logger(__name__)
 
@@ -16,12 +19,26 @@ class Plugin(ExtractionPlugin):
             author=Author('Benjamin Martens', 'benjamin.martens@politie.nl', 'Politie'),
             maturity=MaturityLevel.PROOF_OF_CONCEPT,
             webpage_url='',  # e.g. url to the code repository of your plugin
-            matcher='$data.type=raw',  # add the query for the types of traces your plugin should match
+            matcher='persist-root AND $data.type=raw',  # add the query for the types of traces your plugin should match
             license='Apache License 2.0'
         )
         return plugin_info
 
     def process(self, trace, data_context):
+        file = trace.open()
+        # Store file contents in memory
+        parsed_data = json.load(file)
+        # Parse engine part as json
+        engine_data = json.loads(parsed_data["engine"])
+        # Get accounts
+        accounts = engine_data['backgroundState']['AccountTrackerController']['accounts'].keys()
+
+        # Add accounts to trace
+        for account in accounts:
+            trace.update("account.type", "Ethereum")
+            trace.update("account.name", account)
+            logging.info(f"Found wallet address: {account}")
+
         log.info(f"processing trace {trace.get('name')}")
         # Add your plugin implementation here
 
@@ -30,4 +47,7 @@ if __name__ == '__main__':
     # optional main method to run your plugin with Hansken.py
     # see detail at:
     #  https://netherlandsforensicinstitute.github.io/hansken-extraction-plugin-sdk-documentation/latest/dev/python/hanskenpy.html
-    run_with_hanskenpy(Plugin)
+    run_with_hanskenpy(Plugin,
+                       endpoint='http://localhost:9091/gatekeeper/',
+                       keystore='http://localhost:9090/kestore/',
+                       project='ab73dccd-eea5-4025-8de8-670f3f2c5e5f')
